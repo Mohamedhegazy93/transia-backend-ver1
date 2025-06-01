27101cc7075be3ceac99b08e7f971b35b19c1cb7
unknown <mohamedhegazy49644@gmail.com>
2025-06-02 01:00:02 +0300

    commit

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -6,7 +6,9 @@ import ApiError from "../utils/apiError.js";
 
 export const protectedRoute = async (req, res, next) => {
  
-    const token = req.cookies.accessToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+}
     if (!token) {
       return next(new ApiError("لا يمكنك تنفيذ هذا الطلب", 401));
     }
